4bc4272582bb0ad7accf0ebd24859e66deee66a4
Adriana Tarquino <atarquinocespedes@gmail.com>
2022-04-10 01:16:49 -0400

    Iniciando interfaz de usuario parte 2

--- src/presenter.js
+++ src/presenter.js
@@ -1,13 +1,16 @@
-import convertirCodigoSecreto from "./convertirCodigoSecreto.js";
+import Juego from "./convertirCodigoSecreto.js";
 
 const codigo_imput = document.querySelector("#codigosecreto");
 const form = document.querySelector("#Juego-form");
 const div = document.querySelector("#resultado-div");
+const div2 = document.querySelector("#contar");
 
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    const codigosecreto= convertirCodigoSecreto(codigo_imput.value);
+    const jugar = new Juego();
+    const codigosecreto= jugar.convertirCodigoSecreto(codigo_imput.value);
+    const contar = jugar.contarCantidadDeDigitos(codigo_imput.value);
     div.innerHTML = "<p>" + codigosecreto + "</p>";
-    
-  });
+    div2.innerHTML = "<p>" + "Cantidad de Digitos:" + contar + "</p>";
+});
