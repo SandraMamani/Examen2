a27737d6abafd0e6a510b3e09658479af0ea7031
“Sandra <sandra.mamani@ucb.edu.bo>
2022-04-10 01:46:46 -0400

    iniciando interfaz de usuario parte 3

--- src/presenter.js
+++ src/presenter.js
@@ -4,6 +4,11 @@ const codigo_imput = document.querySelector("#codigosecreto");
 const form = document.querySelector("#Juego-form");
 const div = document.querySelector("#resultado-div");
 const div2 = document.querySelector("#contar");
+const Intentos_imput = document.querySelector("#intento");
+const form2 = document.querySelector("#Jugador2-form");
+const div3 = document.querySelector("#respuesta-div");
+
+
 
 
 form.addEventListener("submit", (event) => {
@@ -13,4 +18,11 @@ form.addEventListener("submit", (event) => {
     const contar = jugar.contarCantidadDeDigitos(codigo_imput.value);
     div.innerHTML = "<p>" + codigosecreto + "</p>";
     div2.innerHTML = "<p>" + "Cantidad de Digitos:" + contar + "</p>";
+});
+
+form2.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const intento2 = Intentos_imput.value;
+    div3.innerHTML = "<p>" + intento2 + "</p>";
+    
 });
